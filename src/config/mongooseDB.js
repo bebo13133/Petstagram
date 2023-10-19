673bf3c47fa98802e3752f6310d8e28bb9476851
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-10-19 19:34:45 +0300

    Update mongooseDB.js

--- src/config/mongooseDB.js
+++ src/config/mongooseDB.js
@@ -2,11 +2,17 @@ const mongoose = require('mongoose')
 
 
 //TODO: CHANGE DB NAME mongodb://127.0.0.1:27017/name - заместваме според задачата
-const uriPets = 'mongodb://127.0.0.1:27017/petstagram'
+const uriPets = 'mongodb://localhost:27017/petstagram'
 
 async function connectDB (){
 
-await mongoose.connect(uriPets)
-
+await mongoose.connect(uriPets,function(err, db) {
+    if (err) {
+      throw err;
+    }
+    console.log('db connected')
+    db.close()
+  })
 }
 module.exports = connectDB
+
