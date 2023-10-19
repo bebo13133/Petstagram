b7c4873494e026339e6ce836d54346dd4d813cb1
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-10-19 22:22:22 +0300

    Update errorHelpers.js

--- src/utils/errorHelpers.js
+++ src/utils/errorHelpers.js
@@ -1,13 +1,13 @@
-const { MongooseError, Error } = require('mongoose')
+// const { MongooseError, Error } = require('mongoose')
 
 
-exports.extractErrorMessage = (error) => {
+// exports.extractErrorMessage = (error) => {
 
-    if (error instanceof MongooseError || error instanceof Error) {
-        return Object.values(error.errors).map(err => err.message)
-    } else if (error) {
-        return [error.message]
-    }
+//     if (error instanceof MongooseError || error instanceof Error) {
+//         return Object.values(error.errors).map(err => err.message)
+//     } else if (error) {
+//         return [error.message]
+//     }
     
     
-}
+// }
